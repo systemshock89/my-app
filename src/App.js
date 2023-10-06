a25ccb2e7d895c85eb65ead908301c27adee1499
systemshock89 <666qqq77>
2023-10-06 18:50:45 +0300

    react state, .setState()

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+
+// function WhoAmI (props) {
+  
+// function WhoAmI ({name, surname, link}) {
+//   return (
+//     <div>
+//       <h1>My name is {name()}, surname - {surname}</h1>
+//       <a href="{link}">My profile</a>
+//     </div>
+//   )
+// }
+
+// классовый компонент
+class WhoAmI extends Component {
+  // для передачи в экземпляр класса передать пропасы, используем конструктор (эта возможность приходит из react Component)
+  constructor(props){
+    super(props);
+
+    // состояние - объект, кот-й описывает динамические вещи, кот-е будут в объекте
+    this.state = {
+      years: 27,
+      text: '+++'
+    }
+  }
+  // если в конструкторе больше ничего нет (кроме super(props); ), то его можно удалить
+
+
+  // метод
+  nextYear = () => {
+    // изменяем состояние:
+
+    // this.setState({
+      // так правильно. Но лучше передадим callback ф-ю, чтобы счетчик не сбился
+      // years: this.state.years + 1 
+
+      // years: ++this.state.years и так нельзя, тк тогда мы поменяем предыдущее состояние
+    // })
+
+    // this.state.years++; и так не правильно изменять состояние!
+
+    // тк у нас состояние счетчика зависит от предыдущиго его знач-я,
+    // передадим callback ф-ю, чтобы счетчик не сбился
+    // а если нам не важно предыдущее знач-е (напр вводим текст в input), то просто передадим объект, а не ф-ю
+    this.setState(state => {
+      years: state.years + 1 // уже без this.
+    }) // сокращенние написание: здесь оборачиваем (), заменяя слово return
+  }
+
+  render(){
+    const {name, surname, link} = this.props;
+
+    return (
+      <div>
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+        <a href={link}>My profile</a>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WhoAmI name='Jonh' surname="Smith" link="facebook.com"/>
+      <WhoAmI name='Alex' surname="Smith" link="vk.com"/>
+      
+      {/* <WhoAmI name={() => {return 'Jonh1'}} surname="Smith" link="facebook.com"/> */}
+      {/* <WhoAmI name={{firstName: 'Jonh'}} surname="Smith" link="facebook.com"/> */}
+      {/* <WhoAmI name="Jonh" surname="Smith" link="facebook.com"/> */}
     </div>
   );
 }
