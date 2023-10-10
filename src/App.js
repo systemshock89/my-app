45f958c4df33ba0e7274ed946bab24408354b950
systemshock89 <system.shock89@gmail.com>
2023-10-10 23:23:17 +0300

    add styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 // import logo from './logo.svg';
 import { Component } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
+
+const EmpItem = styled.div`
+  padding: 20px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+  a {
+    display: block;
+    margin: 10px 0;
+    color: ${props => props.active ? 'orange' : 'black'};
+  }
+  input {
+    display: block;
+    margin-top: 10px;
+  }
+`;
+
+const Header = styled.h2`
+  fonst-size: 22px;
+`;
+
+export const Button = styled.button`
+  display: block;
+  padding: 5px 15px;
+  background-color: gold;
+  border: 1px solid rgba(0,0,0, .2);
+  box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+`;
 
 // function WhoAmI (props) {
   
@@ -76,14 +106,14 @@ class WhoAmI extends Component {
     const {position, years, text} = this.state;
     
     return (
-      <>
-        <button onClick={this.nextYear}>{text}</button>
+      <EmpItem active>
+        <Button onClick={this.nextYear}>{text}</Button>
         {/* <button onClick={() => {this.nextYear()}}>{text}</button> */}
-        <h1>
+        <Header>
           My name is {name}, surname - {surname}, 
           age - {years}, 
           positon - {position}
-        </h1>
+        </Header>
         <a href={link}>My profile</a>
 
         <form>
@@ -93,21 +123,27 @@ class WhoAmI extends Component {
           {/* Чтобы передать аргумент в обработчик событий, нужно использовать стрелочную ф-ю:  */}
           <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
         </form>
-      </>
+      </EmpItem>
     )
   }
 }
 
+// styled components
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 80px auto 0 auto;
+`;
+
 function App() {
   return (
-    <div className="App">
+    <Wrapper>
       <WhoAmI name='Jonh' surname="Smith" link="facebook.com"/>
       <WhoAmI name='Alex' surname="Smith" link="vk.com"/>
       
       {/* <WhoAmI name={() => {return 'Jonh1'}} surname="Smith" link="facebook.com"/> */}
       {/* <WhoAmI name={{firstName: 'Jonh'}} surname="Smith" link="facebook.com"/> */}
       {/* <WhoAmI name="Jonh" surname="Smith" link="facebook.com"/> */}
-    </div>
+    </Wrapper>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import { createRoot } from "react-dom/client";
+import styled from 'styled-components';
 import App from './App';
 
 import "./index.css";
+import { Button } from "./App";
+import { StrictMode } from "react";
+
+const BigButton = styled(Button)`
+    margin: 0 auto;
+    width: 245px;
+    text-align: center;
+`;
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+    <StrictMode>
+        <App />
+        <BigButton as="a">Отправить отчет</BigButton>
+    </StrictMode>
+);
